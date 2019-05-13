edacc775abce9f719072b425ed9e9766cb829c59
justin <jlouie10@gmail.com>
2019-05-12 22:07:55 -0500

    Added concert-this function to return artist query

--- liri.js
+++ liri.js
@@ -4,21 +4,45 @@
 
 // var spotify = new Spotify(keys.spotify);
 
-let command = process.argv[2];
+const axios = require("axios");
+
+let input = process.argv;
+let command = input[2];
+let arg = input.slice(3).join(' ');
 
 switch (command) {
-    case "concert-this":
+    case 'concert-this':
+        console.log('Your concert-this results:')
+        concertThis(arg);
+        break;
+    case 'spotify-this-song':
         console.log(command);
         break;
-    case "spotify-this-song":
+    case 'movie-this':
         console.log(command);
         break;
-    case "movie-this":
-        console.log(command);
-        break;
-    case "do-what-it-says":
+    case 'do-what-it-says':
         console.log(command);
         break;
     default:
-        console.log("Error");
+        console.log('Error');
+}
+
+function concertThis(artist) {
+    const appId = '';
+    let artistEnc = artist.replace(' ', '%20');
+    let queryUrl = 'https://rest.bandsintown.com/artists/' + artistEnc + '/events?app_id=' + appId;
+
+    axios.get(queryUrl)
+        .then(function (response) {
+            response.data.forEach(element => {
+                console.log('\nArtist: ' + artist);
+                console.log('Venue: ' + element.venue.name);
+                console.log('Location: ' + element.venue.city + ', ' + element.venue.region);
+                console.log('Date: ' + element.datetime + '\n');
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
