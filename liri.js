fcc7ae0033893fe5f022cbfa14af224e7df7a0b9
justin <jlouie10@gmail.com>
2019-05-16 16:34:30 -0500

    Added do-what-it-says request to run pre-defined command with search term

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const Spotify = require('node-spotify-api');
 const moment = require('moment');
 const axios = require('axios');
+const fs = require('fs');
 
 let keys = require('./keys.js');
 let spotify = new Spotify(keys.spotify);
@@ -10,106 +11,113 @@ let omdb = keys.omdb;
 
 let input = process.argv;
 let command = input[2];
-let arg = input.slice(3).join(' ');
+let search = input.slice(3).join(' ');
 
-switch (command) {
-    case 'concert-this':
-        concertThis(arg);
-        break;
-    case 'spotify-this-song':
-        spotifyThisSong(arg);
-        break;
-    case 'movie-this':
-        movieThis(arg);
-        break;
-    case 'do-what-it-says':
-        console.log(command);
-        break;
-    default:
-        console.log('Error');
+// Runs function associated with user input command
+let askLiri = (cmd, searchTerm) => {
+    switch (cmd) {
+        case 'concert-this':
+            concertThis(searchTerm);
+            break;
+        case 'spotify-this-song':
+            spotifyThisSong(searchTerm);
+            break;
+        case 'movie-this':
+            movieThis(searchTerm);
+            break;
+        case 'do-what-it-says':
+            doWhatItSays();
+            break;
+        default:
+            console.log('Error');
+    }
 }
 
 // Fetches concert information from Bands in Town
-function concertThis(artist) {
+let concertThis = (artist) => {
     let queryUrl = 'https://rest.bandsintown.com/artists/' + artist + '/?app_id=codingbootcamp';
 
     axiosGet(queryUrl, getArtist);
-}
+};
 
 // Fetches artist to confirm artist exists (allows partial matches), prints artist name if found in the query
-function getArtist(response) {
+let getArtist = (response) => {
     let artistName = response.data.name;
 
     if (artistName === undefined) {
-        console.log('\nLiri could not find this artist.');
+        console.log('\nLiri could not find this artist.\n');
     }
     else {
         let queryUrl = 'https://rest.bandsintown.com/artists/' + artistName + '/events/?app_id=codingbootcamp';
 
         axiosGet(queryUrl, printConcert, { artist: artistName });
     }
-}
+};
 
 // Prints concert information to console
-function printConcert(response, params) {
+let printConcert = (response, params) => {
     if (response.data.length > 0) {
+        console.log('_\n')
+
         response.data.forEach(element => {
-            console.log('\nArtist: ' + params.artist);
+            console.log('Artist: ' + params.artist);
             console.log('Venue: ' + element.venue.name);
             console.log('Location: ' + element.venue.city + ', ' + element.venue.region);
-            console.log('Date: ' + moment(element.datetime).format('MM/DD/YYYY'));
+            console.log('Date: ' + moment(element.datetime).format('MM/DD/YYYY') + '\n');
         });
     }
     else {
-        console.log('\nLiri could not find any concerts.');
+        console.log('\nLiri could not find any concerts.\n');
     }
-}
+};
 
 // Fetches movie information from OMDB
-function movieThis(movie) {
+let movieThis = (movie) => {
     let queryUrl = 'http://www.omdbapi.com/?t=' + movie + '&plot=full&apikey=' + omdb.apiKey;
 
     axiosGet(queryUrl, printMovie);
+};
 
-    // Prints movie information to console
-    function printMovie(response) {
-        console.log('\nTitle: ' + response.data.Title);
-        console.log('Year: ' + response.data.Year);
-        console.log('Ratings: ');
+// Prints movie information to console
+let printMovie = (response) => {
+    console.log('\nTitle: ' + response.data.Title);
+    console.log('Year: ' + response.data.Year);
+    console.log('Ratings: ');
 
-        response.data.Ratings.forEach(element => {
-            if (element.Source === 'Internet Movie Database') {
-                console.log('  IMDB: ' + element.Value);
-            }
-            else if (element.Source === 'Rotten Tomatoes') {
-                console.log('  Rotten Tomatoes: ' + element.Value);
-            }
-        });
+    response.data.Ratings.forEach(element => {
+        if (element.Source === 'Internet Movie Database') {
+            console.log('  IMDB: ' + element.Value);
+        }
+        else if (element.Source === 'Rotten Tomatoes') {
+            console.log('  Rotten Tomatoes: ' + element.Value);
+        }
+    });
 
-        console.log('Country: ' + response.data.Country);
-        console.log('Language: ' + response.data.Language);
-        console.log('Plot: ' + response.data.Plot);
-        console.log('Actors: ' + response.data.Actors);
-    }
-}
+    console.log('Country: ' + response.data.Country);
+    console.log('Language: ' + response.data.Language);
+    console.log('Plot: ' + response.data.Plot);
+    console.log('Actors: ' + response.data.Actors + '\n');
+};
 
 // GET request using Axios 
-function axiosGet(url, callback, params) {
+let axiosGet = (url, callback, params) => {
     axios.get(url)
         .then(function (response) {
             callback(response, params);
         })
         .catch(function (error) {
-            console.log('\nLiri could not complete your request. ' + error);
+            console.log('\nLiri could not complete your request. ' + error + '\n');
         });
-}
+};
 
 // Fetches song information from Spotify
-function spotifyThisSong(song) {
+let spotifyThisSong = (song) => {
     spotify.search({ type: 'track', query: song }, function (err, data) {
         if (err) {
-            return console.log('\nYour spotify-this-song request could not be completed. ' + err);
+            return console.log('\nYour spotify-this-song request could not be completed. ' + err + '\n');
         }
+
+        console.log('_\n')
 
         data.tracks.items.forEach(track => {
 
@@ -126,7 +134,22 @@ function spotifyThisSong(song) {
             console.log('Artist(s): ' + artists);
             console.log('Song: ' + track.name);
             console.log('Album: ' + track.album.name);
-            console.log('Url: ' + track.external_urls.spotify);
+            console.log('Url: ' + track.external_urls.spotify + '\n');
         });
     });
-}
+};
+
+// Reads random.txt and runs command with search term in file
+let doWhatItSays = () => {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log('\nLiri could not complete your request.' + error);
+        }
+
+        let dataArr = data.split(",");
+
+        askLiri(dataArr[0], dataArr[1]);
+    });
+};
+
+askLiri(command, search);
